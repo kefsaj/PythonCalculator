e9c54eadafbc8068a08bca3171db1994c3dfe5f6
Kefin Sajan <kefsaj@github.com>
2021-03-23 00:02:18 -0400

    Fix: Calculator bug

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,6 +1,46 @@
-from Calculator.Subtraction import subtraction
-from Calculator.Addition import addition
+from Calculator.subtraction import subtraction
+from Calculator.addition import addition
+from Calculator.multiply import multiply
+from Calculator.division import division
+from Calculator.square import square
+from Calculator.squareroot import squareroot
 
+#Python program
+
+
+# add function
+
+def addition(n1, n2):
+    return n1 + n2
+
+# Subract function
+
+def subtraction(n1, n2):
+    return n1 - n2
+
+# Multi Function
+
+def multiply(n1, n2):
+    return n1 * n2
+
+# divide function
+
+def division(a, b):
+    a = float(a)
+    b = float(b)
+    return round((b/ a), 9)
+
+# square function
+
+def square(a):
+    a = float(a)
+    return a**2
+
+# squareroot function
+
+def squareroot(a):
+    a = float(a)
+    return round((a**.5), 4)
 
 class Calculator:
     result = 0
@@ -8,10 +48,27 @@ class Calculator:
     def __init__(self):
         pass
 
-    def add(self, a, b):
-        self.result = addition(a, b)
+    def addition(self, a, b):
+        self.result = addition(a,b)
+        return addition(a,b);
+
+    def subtraction(self, a, b):
+        self.result = subtraction(a,b)
+        return subtract(a,b);
+
+    def multiply(self, a, b):
+        self.result = multiply(a, b)
         return self.result
 
-    def subtract(self, a, b):
-        self.result = subtraction(a, b)
+    def divide(self, a, b):
+        self.result = division(a, b)
         return self.result
+
+    def squareroot(self, a):
+        self.result = squareroot(a)
+        return self.result
+
+    def square(self, a):
+        self.result = square(a)
+        return self.result
+
